94226bc1bb29edfdfc3804a4dff9dcf729d0a6ed
wittg3n <wittg3ns@gmail.com>
2024-09-30 05:20:29 +0330

    cleanup

--- config/logger.js
+++ config/logger.js
@@ -1,4 +1,13 @@
 const winston = require('winston');
+const path = require('path');
+
+
+const logDir = path.join(__dirname, '../logs');
+const fs = require('fs');
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
@@ -6,10 +15,26 @@ const logger = winston.createLogger({
         winston.format.json()
     ),
     transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' })
+        new winston.transports.Console({
+            format: winston.format.simple()
+        }),
+        new winston.transports.File({
+            filename: path.join(logDir, 'error.log'),
+            level: 'error',
+            handleExceptions: true
+        }),
+        new winston.transports.File({
+            filename: path.join(logDir, 'combined.log')
+        })
     ],
+    exitOnError: false,
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+process.on('uncaughtException', (error) => {
+    logger.error('Uncaught Exception thrown:', error);
 });
 
 module.exports = logger;
--- src/bot/handler/mainHandler.js
+++ src/bot/handler/mainHandler.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs')
 const path = require('path');
 const { downloadFile } = require('../utils/downloader');
-const responses = require('../../../responses')
+const responses = require('../../../config/responses')
 module.exports = {
     start: (bot) => {
         bot.start((ctx) => {
@@ -48,39 +48,29 @@ module.exports = {
             }
 
             const audioFileId = ctx.message.audio.file_id;
+            console.log(ctx.message.audio.file_name)
             const telegramId = ctx.from.id;
-
             try {
-                // Construct expected file path
                 const expectedAudioFilePath = path.join(__dirname, '../../../userdata', String(telegramId), `${audioFileId}.mp3`);
-
-                // Ensure that each user's session is unique and reset if needed
                 if (ctx.session.audioFileId !== audioFileId) {
                     ctx.session.audioFileId = audioFileId;
-                    ctx.session.audioFilePath = null; // Reset stored audio path
+                    ctx.session.audioFilePath = null;
                 }
-
-                // Check if file already exists
-                if (!ctx.session.audioFilePath || !fs.existsSync(expectedAudioFilePath)) {
+                if (!fs.existsSync(expectedAudioFilePath)) {
                     const sentMessage = await ctx.reply(responses.downloading);
                     ctx.session.messageToDelete = sentMessage.message_id;
 
-                    // Download new file
                     const audioFilePath = await downloadFile(audioFileId, telegramId);
-                    console.log(`Downloaded audio to: ${audioFilePath}`);
+                    console.log(audioFilePath);
                     ctx.session.audioFilePath = audioFilePath;
                 } else {
                     console.log('Audio file already exists, using existing file.');
                     ctx.session.audioFilePath = expectedAudioFilePath;
                 }
-
-                // Delete the downloading message
                 if (ctx.session.messageToDelete) {
                     await ctx.deleteMessage(ctx.session.messageToDelete);
                     ctx.session.messageToDelete = null;
                 }
-
-                // Send the audio back to the user
                 const message = await ctx.replyWithAudio(
                     ctx.session.audioFileId,
                     {
@@ -93,14 +83,12 @@ module.exports = {
                     }
                 );
 
-                // Store the message and chat details in session
                 ctx.session.messageId = message.message_id;
                 ctx.session.chatId = ctx.chat.id;
-
             } catch (error) {
                 console.error('Error handling audio:', error.message);
                 await ctx.reply(responses.failedToProcessAudio);
             }
         });
-    }
+    },
 };
